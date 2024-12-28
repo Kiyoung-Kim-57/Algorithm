b800771e2cc4716804ff8ea011f36b77d0e3629a
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2024-12-28 16:16:38 +0900

    Time: 0 ms (100%), Space: 17.4 MB (100%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.swift
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.swift
@@ -1,23 +1,20 @@
 class Solution {
     func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
-        let uniqueLetters = Array(Set(letters)).sorted()
         var low = 0
-        var high = uniqueLetters.count - 1
+        var high = letters.count - 1
 
         while low <= high {
             let mid = (low + high) / 2
 
-            if target > uniqueLetters[mid] {
+            if target >= letters[mid] {
                 low = mid + 1
                 continue
-            } else if target < uniqueLetters[mid] {
+            } else if target < letters[mid] {
                 high = mid - 1
                 continue
-            } else {
-                return mid + 1 < uniqueLetters.count ? uniqueLetters[mid + 1] : uniqueLetters[0]
-            }
+            } 
         }
 
-        return low <= uniqueLetters.count - 1 ? uniqueLetters[low] :  uniqueLetters[0]
+        return low <= letters.count - 1 ? letters[low] :  letters[0]
     }
 }
