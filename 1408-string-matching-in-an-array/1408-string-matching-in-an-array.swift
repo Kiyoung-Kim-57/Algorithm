2937e31f43a98b3a0657d08ab0eb2c2389b5f9d1
Kiyoung <121777185+Kiyoung-Kim-57@users.noreply.github.com>
2025-01-07 14:32:17 +0900

    Time: 308 ms (8.7%), Space: 17.8 MB (95.65%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.swift
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.swift
@@ -2,10 +2,10 @@ class Solution {
     func stringMatching(_ words: [String]) -> [String] {
         var result: [String] = []
 
-        for word in words {
+        for word in words.sorted(by: { $0.count < $1.count }) {
             var isSubstring: Bool = false
 
-            for target in words {
+            for target in words.sorted(by: { $0.count > $1.count }) {
                 guard target != word else { continue }
 
                 if target.contains(word) {
